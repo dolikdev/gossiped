c732779c4f35f55aa3fc1993538c067ac9cf1d6d
Andriy Dzedolik <dz@dolik.dev>
2026-01-18 15:58:12 +0100

    feat: add support for CP895 (Kamenický) encoding

--- pkg/utils/encoding_test.go
+++ pkg/utils/encoding_test.go
@@ -12,6 +12,9 @@ func TestCharmapEqual(t *testing.T) {
 		g.It("check cp866", func() {
 			g.Assert(DecodeCharmap("\x92\xa5\xe1\xe2", "CP866")).Equal("Тест")
 		})
+		g.It("check cp895", func() {
+			g.Assert(DecodeCharmap("\x80\x81\x82\x83", "CP895")).Equal("Čěšý")
+		})
 		g.It("check utf-8", func() {
 			g.Assert(DecodeCharmap("Тест", "UTF-8")).Equal("Тест")
 		})
@@ -19,6 +22,9 @@ func TestCharmapEqual(t *testing.T) {
 	g.Describe("Check EncodeCharmap()", func() {
 		g.It("check cp866", func() {
 			g.Assert(EncodeCharmap("Тест", "CP866")).Equal("\x92\xa5\xe1\xe2")
+		})
+		g.It("check cp895", func() {
+			g.Assert(EncodeCharmap("Čěšý", "CP895")).Equal("\x80\x81\x82\x83")
 		})
 		g.It("check utf-8", func() {
 			g.Assert(EncodeCharmap("Тест", "UTF-8")).Equal("Тест")
